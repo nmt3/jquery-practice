30d840f14d24391e45b38d89eba14ed1f464cbd5
nmt3 <nmttkm3@gmail.com>
2023-04-27 10:17:40 +0000

    anumation lesson2

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 $(function(){
-  $('.box1').slideDown();
+  $('.box1').slideDown(function(){
+  $('.box1').css({
+    'background-color': '#0000ff',
+    'widht': '200px',
+    'height': '100px'
+    }).slideUp(3000);
+  });
 });
